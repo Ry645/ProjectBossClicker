4fcce50527ec6344ba7e97bc0c16e3b6b202d04a
Ryan Sexton <ry645.dev@gmail.com>
2024-07-15 21:44:00 -0400

    added and imported a bunch of stat systems

---
+++ healthSystem/health_system.gd
@@ -0,0 +1,25 @@
+extends Node
+
+class_name HealthSystem
+
+signal die
+signal healthUpdate(minHealth, maxHealth, currentHealth)
+
+@export var maxHealth:float = 20.0
+@export var minHealth:float = 0.0
+@export var health:float = 20.0
+
+@export var hasGodMode:bool = false
+
+func _ready():
+	emit_signal("healthUpdate", minHealth, maxHealth, health)
+
+func takeDamage(damage):
+	if hasGodMode:
+		return
+	
+	health -= damage
+	#print("health: ", health)
+	emit_signal("healthUpdate", minHealth, maxHealth, health)
+	if health <= minHealth:
+		emit_signal("die")
---
+++ manaSystem/mana_system.gd
@@ -0,0 +1,53 @@
+extends Node
+
+class_name ManaSystem
+
+signal manaUpdate(minMana, maxMana, currentMana)
+signal addToMagicExtract(mana)
+
+@export var maxMana:float = 100.0
+@export var minMana:float = 0.0
+@export var mana:float = 100.0
+
+@export var regenPerSec:float = 5
+
+@export var hasGodMode:bool = false
+
+func _ready():
+	emit_signal("manaUpdate", minMana, maxMana, mana)
+	
+	#TEST
+	startRegenTimer()
+
+func useMana(manaUsed) -> bool:
+	if hasGodMode:
+		manaUsed = 0
+	
+	if mana < manaUsed:
+		return false
+	
+	changeMana(-manaUsed)
+	return true
+
+func changeMana(amount:float): #main function used for altering mana
+	var previousMana:float = mana
+	mana += amount
+	if mana > maxMana:
+		emit_signal("addToMagicExtract", maxMana-previousMana)
+		mana = maxMana
+	
+	emit_signal("manaUpdate", minMana, maxMana, mana)
+	print("mana: ", mana)
+
+func regen():
+	changeMana(regenPerSec)
+
+func startRegenTimer():
+	($regenTimer as Timer).start()
+
+func stopRegenTimer():
+	($regenTimer as Timer).stop()
+
+
+func _on_regen_timer_timeout():
+	regen()

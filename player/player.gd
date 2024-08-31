955a80f119dc337ad0a7b1597377629728fe2356
Ryan Sexton <ry645.dev@gmail.com>
2024-08-30 21:56:05 -0400

    added particle effects to a boss hit and fumbled around with shaders

--- player/player.gd
+++ player/player.gd
@@ -12,6 +12,8 @@ var attackPower = 5
 #TODO will tie to holding items later
 var manaUsage = 5
 
+@export var attackParticlesScene:PackedScene
+
 func _process(_delta):
 	inputProcess()
 
@@ -22,9 +24,16 @@ func inputProcess():
 	if Input.is_action_just_pressed("attack"):
 		for collision in ($playerCollision as Area2D).get_overlapping_areas():
 			if collision.has_method("takeDamage"):
-				collision.takeDamage(attackPower)
+				attack(collision)
 	if Input.is_action_just_pressed("useItem"):
 		useMana() # TODO change to useItem()
 
 func useMana():
 	$manaSystem.useMana(manaUsage)
+
+func attack(collision):
+	collision.takeDamage(attackPower)
+	var node:GPUParticles2D = attackParticlesScene.instantiate()
+	add_child(node)
+	node.emitting = true
+	
